cd9ea2c8fa08c5b93ac63f646a73dd9732f1e835
wjt <wjt@wjt.com>
2019-06-27 21:39:20 +0800

    分支2第二次修改

---
+++ src/main/java/com/qf/entity/s.java
@@ -0,0 +1,10 @@
+package com.qf.entity;
+
+/**
+ * @Author WJT
+ * @Date 2019/6/27 21:36
+ * @Version 1.0
+ **/
+
+public class s {
+}
--- src/test/java/Test2.java
+++ src/test/java/Test2.java
@@ -1,3 +1,4 @@
 public class Test2 {
     int a;
+    int b;
 }
